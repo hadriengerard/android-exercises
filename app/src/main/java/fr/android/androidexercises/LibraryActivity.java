829bb98d1079db2281363059fe88cc8619d854da
Benjamin LACROIX <b.lacroix@meetic-corp.com>
2015-10-25 18:18:24 +0100

    Exercise 2

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -3,9 +3,14 @@ package fr.android.androidexercises;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -14,11 +19,25 @@ public class LibraryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-        TextView messageTextView = (TextView) findViewById(R.id.messageTextView);
-        // TODO call setText() on messageTextView
-
         setSupportActionBar(toolbar);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final URLConnection connection = new URL("https://github.com/blacroix/android-exercises").openConnection();
+                    connection.connect();
+                } catch (IOException e) {
+                    Log.e(LibraryActivity.class.getSimpleName(), "Cannot get URL", e);
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ((TextView) findViewById(R.id.messageTextView)).setText(android.R.string.ok);
+                    }
+                });
+            }
+        }).start();
     }
 
     @Override
