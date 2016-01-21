5c3a01b9fac0afbf284f1b51d3b4c8fc817a8196
Hadrien Gerard <Hadrien Gerard>
2016-01-21 10:21:20 +0100

    exercice 1: ajouter et afficher une ressource de type String

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -17,6 +17,7 @@ public class LibraryActivity extends AppCompatActivity {
 
         TextView messageTextView = (TextView) findViewById(R.id.messageTextView);
         // TODO call setText() on messageTextView
+        messageTextView.setText(R.string.bonjour);
 
         setSupportActionBar(toolbar);
     }
