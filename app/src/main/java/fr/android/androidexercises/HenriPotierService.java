7008d153ef295832e2d9662bc3b74729e5bcbb10
Hadrien Gerard <Hadrien Gerard>
2016-01-22 11:32:04 +0100

    Exercice 'retrofit': utilisation de la library retrofit pour requeter une API REST et utilisation de Timber pour logger les resultats

--- app/src/main/java/fr/android/androidexercises/HenriPotierService.java
+++ app/src/main/java/fr/android/androidexercises/HenriPotierService.java
@@ -1,7 +1,15 @@
 package fr.android.androidexercises;
 
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.http.GET;
+
 public interface HenriPotierService {
 
     // TODO Method GET books which return a List<Book>
+    @GET("/books")
+    Call<List<Book>> listBooks();
+
 
 }
--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -3,6 +3,13 @@ package fr.android.androidexercises;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 import timber.log.Timber;
 
 public class LibraryActivity extends AppCompatActivity {
@@ -16,12 +23,36 @@ public class LibraryActivity extends AppCompatActivity {
         Timber.plant(new Timber.DebugTree());
 
         // TODO build Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://henri-potier.xebia.fr/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
         // TODO create a service
+        HenriPotierService service = retrofit.create(HenriPotierService.class);
 
         // TODO listBooks()
+        Call<List<Book>> call = service.listBooks();
 
         // TODO enqueue call and display book title
+        //final List<Book> books;
+        call.enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Response<List<Book>> response, Retrofit retrofit) {
+                // TODO success
+
+                for (Book b : response.body()) {
+                    Timber.i(b.getTitle());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                // TODO error occurred
+                Timber.e(t, "Une erreur est survenue");
+            }
+        });
 
         // TODO log books
 
